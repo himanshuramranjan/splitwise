5051c846e39ee0925445e5e73c9be841abce2413
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-07-27 22:54:44 +0530

    class modified with refactoring

--- src/models/Expense.java
+++ src/models/Expense.java
@@ -1,7 +1,5 @@
 package models;
 
-import strategies.SplitStrategy;
-
 import java.util.List;
 import java.util.UUID;
 
@@ -9,39 +7,37 @@ public class Expense {
     private final String id;
     private final User paidBy;
     private double amount;
-    private final List<Split> splits;
-    private final SplitStrategy splitStrategy;
+    private List<User> involvedUsers;
+    private List<Split> splits;
 
-    public Expense(User paidBy, double amount, List<Split> splits, SplitStrategy splitStrategy) {
+    public Expense(User paidBy, double amount, List<User> involvedUsers) {
         this.id = UUID.randomUUID().toString();
         this.paidBy = paidBy;
         this.amount = amount;
-        this.splits = splits;
-        this.splitStrategy = splitStrategy;
-    }
-
-    public boolean validate() {
-        return this.splitStrategy.validate(this);
+        this.involvedUsers = involvedUsers;
     }
 
     public double getAmount() {
         return amount;
     }
-
     public String getId() {
         return id;
     }
-
     public User getPaidBy() {
         return paidBy;
     }
-
     public List<Split> getSplits() {
         return splits;
     }
+    public void setSplits(List<Split> splits) {
+        this.splits = splits;
+    }
+    public List<User> getInvolvedUsers() {
+        return involvedUsers;
+    }
 
-    public SplitStrategy getSplitStrategy() {
-        return splitStrategy;
+    public void setInvolvedUsers(List<User> involvedUsers) {
+        this.involvedUsers = involvedUsers;
     }
 
     public void setAmount(double amount) {
