de338c34e7e944bc90e68d50171f7bbd40c59e7b
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-07-27 22:57:15 +0530

    new demo added for thorough testing

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,43 @@
 import models.Expense;
 import models.Group;
-import models.Split;
 import models.User;
-import service.GroupManager;
+import service.GroupService;
 import strategies.EqualSplitStrategy;
 import strategies.ExactSplitStrategy;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        GroupManager groupManager = new GroupManager();
+        GroupService groupService = new GroupService();
 
         User u1 = new User("Alice", "alice@example.com");
         User u2 = new User("Bob", "bob@example.com");
         User u3 = new User("Charlie", "charlie@example.com");
 
-        Group group = groupManager.createGroup("Trip to Goa");
-        groupManager.addUser(group.getName(), u1);
-        groupManager.addUser(group.getName(), u2);
-        groupManager.addUser(group.getName(), u3);
+        String tripToGoa = "Trip to Goa";
+        Group group = groupService.createGroup(tripToGoa);
+        groupService.addUser(group.getName(), u1);
+        groupService.addUser(group.getName(), u2);
+        groupService.addUser(group.getName(), u3);
 
-        List<Split> splits1 = Arrays.asList(
-                new Split(u1, 500),
-                new Split(u2, 1000),
-                new Split(u3, 0)
-        );
-        Expense expense1 = new Expense(u1, 1500, splits1, new ExactSplitStrategy());
-        groupManager.addExpense("Trip to Goa", expense1);
+        Expense expense = groupService.addExpense(tripToGoa, u1, List.of(u1, u2), 1500.0, new ExactSplitStrategy(), List.of(500.0, 1000.0));
+        groupService.showBalances(group.getName());
 
-        List<Split> splits2 = Arrays.asList(
-                new Split(u1, 200),
-                new Split(u2, 800),
-                new Split(u3, 100)
-        );
-        Expense expense2 = new Expense(u2, 1000, splits2, new ExactSplitStrategy());
-        groupManager.addExpense(group.getName(), expense2);
+        Expense expense2 = groupService.addExpense(group.getName(), u2, List.of(u1, u2, u3),1100.0, new ExactSplitStrategy(), List.of(200.0, 800.0, 100.0));
+        groupService.showBalances(group.getName());
 
-        groupManager.showBalances(group.getName());
+        groupService.updateExpense(group.getName(), expense.getId(), List.of(u1, u2, u3), 1200.0, new EqualSplitStrategy(), null);
+        groupService.updateExpense(group.getName(), expense2.getId(), List.of(u1, u2, u3), 900.0, new EqualSplitStrategy(), null);
 
-        List<Split> updatedSplits = Arrays.asList(
-                new Split(u1, 100),
-                new Split(u2, 200),
-                new Split(u3, 800)
-        );
-        Expense updatedExpense1 = new Expense(u2, 1100, updatedSplits, new ExactSplitStrategy());
-        groupManager.updateExpense("Trip to Goa", updatedExpense1);
 
         System.out.println("\nUpdated Balances:");
-
-
+        groupService.showBalances(tripToGoa);
         System.out.println("\nSimplified Debts:");
-        groupManager.simplifyDebt("Trip to Goa");
-        groupManager.showBalances("Trip to Goa");
+
+        groupService.simplifyDebt(tripToGoa);
+        groupService.showBalances(tripToGoa);
 
 
     }
