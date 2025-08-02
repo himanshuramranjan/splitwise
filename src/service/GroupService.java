0df77e9d5eae2e58254304ff0f59d91510841a17
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-08-02 22:01:23 +0530

    refactored the Group Service to remove the dependency from the ExpenseService

--- src/service/GroupService.java
+++ src/service/GroupService.java
@@ -2,8 +2,9 @@ package service;
 
 import models.Expense;
 import models.Group;
+import models.Split;
 import models.User;
-import strategies.SplitStrategy;
+import service.strategies.SplitStrategy;
 
 import java.util.HashMap;
 import java.util.List;
@@ -11,17 +12,14 @@ import java.util.Map;
 
 public class GroupService {
     private final Map<String, Group> groups;
-    private final Map<String, ExpenseService> groupExpenseServices;
 
     public GroupService() {
         this.groups = new HashMap<>();
-        this.groupExpenseServices = new HashMap<>();
     }
 
     public Group createGroup(String groupName) {
         Group group = new Group(groupName);
         groups.put(groupName, group);
-        groupExpenseServices.put(groupName, new ExpenseService());
         return group;
     }
 
@@ -34,48 +32,62 @@ public class GroupService {
 
     public Expense addExpense(String groupName, User paidBy, List<User> involvedUsers, double totalAmount, SplitStrategy splitStrategy, Object extraData) {
         Group group = groups.get(groupName);
-        ExpenseService service = groupExpenseServices.get(groupName);
-        if(group != null && service != null) {
-            Expense expense = service.addExpense(paidBy, involvedUsers, totalAmount, splitStrategy, extraData);
-            group.addExpenseId(expense.getId());
+
+        if(group != null) {
+            Expense expense = new Expense(paidBy, totalAmount, involvedUsers);
+            List<Split> splits = splitStrategy.calculateSplit(expense, extraData);
+            expense.setSplits(splits);
+
+            group.getBalanceSheet().updateBalance(expense.getPaidBy(), expense.getSplits());
+            group.getExpenseMap().put(expense.getId(), expense);
+
             return expense;
         }
         return null;
     }
 
     public Expense updateExpense(String groupName, String expenseId, List<User> involvedUsers, double newAmount, SplitStrategy splitStrategy, Object extraData) {
-        ExpenseService service = groupExpenseServices.get(groupName);
-        if(service != null) {
-            return service.updateExpense(expenseId, involvedUsers, newAmount, splitStrategy, extraData);
+        Group group = groups.get(groupName);
+        Expense oldExpense = group.getExpenseMap().get(expenseId);
+
+        if(oldExpense != null) {
+            group.getBalanceSheet().reverseBalance(oldExpense.getPaidBy(), oldExpense.getSplits());
+
+            oldExpense.setInvolvedUsers(involvedUsers);
+            oldExpense.setAmount(newAmount);
+
+            List<Split> splits = splitStrategy.calculateSplit(oldExpense, extraData);
+            oldExpense.setSplits(splits);
+
+            group.getBalanceSheet().updateBalance(oldExpense.getPaidBy(), splits);
+            return oldExpense;
+        } else {
+            throw new IllegalArgumentException("Expense not found : " + expenseId);
         }
-        return null;
     }
 
     public void deleteExpense(String groupName, String expenseId) {
-        ExpenseService service = groupExpenseServices.get(groupName);
-        if(service == null) {
-            throw new IllegalArgumentException("Group not found " + groupName);
-        }
-        service.deleteExpense(expenseId);
+        Group group = groups.get(groupName);
+        Expense expense = group.getExpenseMap().get(expenseId);
+
+        group.getBalanceSheet().reverseBalance(expense.getPaidBy(), expense.getSplits());
+        group.getExpenseMap().remove(expenseId);
     }
 
     public void showBalances(String groupName) {
         Group group = groups.get(groupName);
-        ExpenseService expenseService = groupExpenseServices.get(groupName);
 
-        if(expenseService != null && group != null) {
+        if(group != null) {
             Map<String, User> userMap = new HashMap<>();
             for (User user : group.getUsers()) {
                 userMap.put(user.getUserId(), user);
             }
-            new DisplayService().showBalances(expenseService.getBalances(), userMap);
+            new DisplayService().showBalances(group.getBalanceSheet().getBalances(), userMap);
         }
     }
 
     public void simplifyDebt(String groupName) {
-        ExpenseService service = groupExpenseServices.get(groupName);
-        if(service != null) {
-            new DebtSimplifier().simplify(service.getBalances());
-        }
+        Group group = groups.get(groupName);
+        DebtSimplifier.simplify(group.getBalanceSheet().getBalances());
     }
 }
