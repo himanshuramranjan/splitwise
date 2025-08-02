fd2452ad468c8884aaec7d7f9488e61bc5c2b1ec
Himanshuram Ranjan <himanshuramr@gmail.com>
2025-08-02 22:02:04 +0530

    added the example to display the working of app

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import models.Expense;
 import models.Group;
 import models.User;
 import service.GroupService;
-import strategies.EqualSplitStrategy;
-import strategies.ExactSplitStrategy;
+import service.strategies.EqualSplitStrategy;
+import service.strategies.ExactSplitStrategy;
+import service.strategies.PercentageSplitStrategy;
 
 import java.util.List;
 
@@ -22,23 +23,19 @@ public class Main {
         groupService.addUser(group.getName(), u2);
         groupService.addUser(group.getName(), u3);
 
-        Expense expense = groupService.addExpense(tripToGoa, u1, List.of(u1, u2), 1500.0, new ExactSplitStrategy(), List.of(500.0, 1000.0));
+        Expense expense = groupService.addExpense(tripToGoa, u1, List.of(u1, u2), 1500.0, new PercentageSplitStrategy(), List.of(50, 50));
         groupService.showBalances(group.getName());
 
-        Expense expense2 = groupService.addExpense(group.getName(), u2, List.of(u1, u2, u3),1100.0, new ExactSplitStrategy(), List.of(200.0, 800.0, 100.0));
+        groupService.addExpense(group.getName(), u2, List.of(u1, u2, u3),1100.0, new ExactSplitStrategy(), List.of(200.0, 800.0, 100.0));
         groupService.showBalances(group.getName());
 
         groupService.updateExpense(group.getName(), expense.getId(), List.of(u1, u2, u3), 1200.0, new EqualSplitStrategy(), null);
-        groupService.updateExpense(group.getName(), expense2.getId(), List.of(u1, u2, u3), 900.0, new EqualSplitStrategy(), null);
-
 
         System.out.println("\nUpdated Balances:");
         groupService.showBalances(tripToGoa);
         System.out.println("\nSimplified Debts:");
 
         groupService.simplifyDebt(tripToGoa);
-        groupService.showBalances(tripToGoa);
-
 
     }
 }
